966512a0e219a2dab55dd9467c348e9634196493
fredrikzkl <fredrik_kloster@hotmail.com>
2018-03-23 16:27:34 +0100

    added new enemy

---
+++ SceneObjects/enemyScene/Target1.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+export (int) var move_area = 100
+export (int) var move_speed = 1
+
+
+
+# Initial values
+var x_init = 0
+var y_init = 0
+
+# 
+var increase = true
+
+
+func _ready():
+	set_process(true)
+	x_init = get_position().x
+	y_init = get_position().y
+	
+
+func _process(delta):
+	if(get_position().y > y_init + move_area):
+		increase = false
+	if(get_position().y < y_init - move_area):
+		increase = true
+		
+	if(increase):
+		position.y = get_position().y + move_speed
+	else:
+		position.y = get_position().y - move_speed
+	
+	
+	
